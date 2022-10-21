ceb1e9132ab7c4d4a550fdc9a24c1cf5df53e9f0
maeng0830 <kmk930830@gmail.com>
2022-10-21 22:23:16 +0900

    project: LM. create

---
+++ src/main/java/com/maeng0830/listentothismusic/BT.java
@@ -0,0 +1,8 @@
+package com.maeng0830.listentothismusic;
+
+public class BT {
+
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
---
+++ src/main/java/com/maeng0830/listentothismusic/ListentothismusicApplication.java
@@ -0,0 +1,13 @@
+package com.maeng0830.listentothismusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListentothismusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListentothismusicApplication.class, args);
+	}
+
+}
