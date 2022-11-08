2e9f21e61fe42ab445186e37c3bff94b04400d5a
maeng0830 <kmk930830@gmail.com>
2022-11-09 00:03:45 +0900

    feat: 관리자 회원 목록 조회 api 구현

---
+++ src/main/java/com/maeng0830/listentothismusic/service/AdminService.java
@@ -0,0 +1,26 @@
+package com.maeng0830.listentothismusic.service;
+
+import com.maeng0830.listentothismusic.domain.Member;
+import com.maeng0830.listentothismusic.repository.MemberRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AdminService {
+
+    private final MemberRepository memberRepository;
+
+    // 회원 목록 조회
+    public Page<Member> viewMemberList(Pageable pageable) {
+        int page = (pageable.getPageNumber() == 0) ? 0 : (pageable.getPageNumber() - 1);
+
+        pageable = PageRequest.of(page, 10);
+
+        return memberRepository.findAll(pageable);
+    }
+}
