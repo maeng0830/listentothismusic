2584fd65c0e8389ee1f9e76eefb705bf426fa5d2
maeng0830 <kmk930830@gmail.com>
2022-11-12 23:57:27 +0900

    fix: 게시글 작성 html 수정

--- src/main/java/com/maeng0830/listentothismusic/domain/Post.java
+++ src/main/java/com/maeng0830/listentothismusic/domain/Post.java
@@ -1,5 +1,6 @@
 package com.maeng0830.listentothismusic.domain;
 
+import com.maeng0830.listentothismusic.code.memberCode.PostStatusCode;
 import java.time.LocalDateTime;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -30,10 +31,11 @@ public class Post {
     private String content; // 본문
 
     private String musicLink; // 음악 링크
-    private String musicSubject; // 음악 제목
+    private String musicTitle; // 음악 제목
     private String artist; // 아티스트명
     private LocalDateTime postDtt; // 작성 일시
 
     private LocalDateTime reportDtt; // 신고 일시
-    private boolean reportYn; // 신고 처리 여부
+    private String reportReason; // 신고 사유
+    private PostStatusCode postStatus; // 게시글 상태 코드
 }
