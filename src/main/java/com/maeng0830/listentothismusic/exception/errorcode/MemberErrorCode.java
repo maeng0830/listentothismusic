d074359ee24970ccf15a32e6c607daf209f64677
maeng0830 <kmk930830@gmail.com>
2022-11-21 04:31:47 +0900

    feat: 신고 게시글, 댓글 관리 api 구현

--- src/main/java/com/maeng0830/listentothismusic/exception/errorcode/MemberErrorCode.java
+++ src/main/java/com/maeng0830/listentothismusic/exception/errorcode/MemberErrorCode.java
@@ -13,7 +13,8 @@ public enum MemberErrorCode implements ErrorCode {
     INCORRECT_MEMBER_EMAIL("유효한 이메일이 아닙니다."),
     EXCEED_AUTH_TIME("인증 유효 시간을 초과했습니다. 인증 메일을 재요청 해주세요."),
     REQUIRED_LOGIN("로그인을 해주세요."),
-    NON_EXISTENT_MEMBER("존재하지 않는 회원입니다.");
+    NON_EXISTENT_MEMBER("존재하지 않는 회원입니다."),
+    NON_AUTHORITY("권한이 없습니다.");
 
 
     private final String description;
--- src/main/java/com/maeng0830/listentothismusic/service/AdminService.java
+++ src/main/java/com/maeng0830/listentothismusic/service/AdminService.java
@@ -1,10 +1,18 @@
 package com.maeng0830.listentothismusic.service;
 
+import com.maeng0830.listentothismusic.code.commentCode.CommentStatusCode;
 import com.maeng0830.listentothismusic.code.memberCode.MemberStatusCode;
+import com.maeng0830.listentothismusic.code.postCode.PostStatusCode;
+import com.maeng0830.listentothismusic.domain.Comment;
 import com.maeng0830.listentothismusic.domain.Member;
+import com.maeng0830.listentothismusic.domain.Post;
 import com.maeng0830.listentothismusic.exception.LimuException;
+import com.maeng0830.listentothismusic.exception.errorcode.CommentErrorCode;
 import com.maeng0830.listentothismusic.exception.errorcode.MemberErrorCode;
+import com.maeng0830.listentothismusic.exception.errorcode.PostErrorCode;
+import com.maeng0830.listentothismusic.repository.CommentRepository;
 import com.maeng0830.listentothismusic.repository.MemberRepository;
+import com.maeng0830.listentothismusic.repository.PostRepository;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +26,8 @@ import org.springframework.stereotype.Service;
 public class AdminService {
 
     private final MemberRepository memberRepository;
+    private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
 
     // 회원 목록 조회
     public Page<Member> viewMemberList(Pageable pageable) {
@@ -40,5 +50,44 @@ public class AdminService {
         member.setStatus(MemberStatusCode.valueOf(status));
 
         memberRepository.save(member);
+    }
+
+    // 신고 게시글 목록 조회
+    public Page<Post> viewReportedPostList(Pageable pageable) {
+        return postRepository.findByPostStatus(PostStatusCode.REPORT, pageable);
+    }
+
+    // 신고 게시글 상세 조회
+    public Post viewReportedPostDetail(Long id) {
+
+        return postRepository.findById(id).orElseThrow(() -> new LimuException(PostErrorCode.NON_EXISTENT_POST));
+    }
+
+    // 신고 게시글 정보 수정
+    public void modPostInfo(Long id, PostStatusCode status) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new LimuException(PostErrorCode.NON_EXISTENT_POST));
+
+        post.setPostStatus(status);
+
+        postRepository.save(post);
+    }
+
+    // 신고 댓글 목록 조회
+    public Page<Comment> viewReportedCommentList(Pageable pageable) {
+        return commentRepository.findByCommentStatus(CommentStatusCode.REPORT, pageable);
+    }
+
+    // 신고 댓글 상세 조회
+    public Comment viewReportedCommentDetail(Long id) {
+        return commentRepository.findById(id).orElseThrow(() -> new LimuException(CommentErrorCode.NON_EXISTENT_COMMENT));
+    }
+
+    // 신고 댓글 정보 수정
+    public void modCommentInfo(Long id, CommentStatusCode commentStatus) {
+        Comment comment = commentRepository.findById(id).orElseThrow(() -> new LimuException(CommentErrorCode.NON_EXISTENT_COMMENT));
+
+        comment.setCommentStatus(commentStatus);
+
+        commentRepository.save(comment);
     }
 }
